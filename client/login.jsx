6f949b7d4642a5545f39b53dffb5bc9d4befda25
mksigrest <mksigrest@gmail.com>
2025-11-15 20:25:00 -0500

    added handleSignup function

--- client/login.jsx
+++ client/login.jsx
@@ -17,3 +17,25 @@ const handleLogin = (e) => {
     helper.sendPost(e.target.action, { username, pass });
     return false;
 }
+
+const handleSignup = (e) => {
+    e.preventDefault();
+    helper.hideError();
+
+    const username = e.target.querySelector('#user').value;
+    const pass = e.target.querySelector('#pass').value;
+    const pass2 = e.target.querySelector('#pass2').value;
+
+    if (!username || !pass || !pass2) {
+        helper.handleError('All fields are required!');
+        return false;
+    }
+
+    if (pass !== pass2) {
+        helper.handleError('Passwords do not match!');
+        return false;
+    }
+
+    helper.sendPost(e.target.action, { username, pass, pass2 });
+    return false;
+}

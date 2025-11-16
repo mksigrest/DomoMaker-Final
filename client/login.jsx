55c6629f36033301b6a3910ab3c11cae6a506030
mksigrest <mksigrest@gmail.com>
2025-11-15 20:20:07 -0500

    base login.jsx

---
+++ client/login.jsx
@@ -0,0 +1,19 @@
+const helper = require('./helper.js');
+const React = require('react');
+const { createRoot } = require('react-dom/client');
+
+const handleLogin = (e) => {
+    e.preventDefault();
+    helper.hideError();
+
+    const username = e.target.querySelector('#user').value;
+    const pass = e.target.querySelector('#pass').value;
+
+    if (!username || !pass) {
+        helper.handleError('Username or password is empty!');
+        return false;
+    }
+
+    helper.sendPost(e.target.action, { username, pass });
+    return false;
+}

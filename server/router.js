1b42de1f7ca82a0cd38e62d59a5ca0dbc0993b51
mksigrest <mksigrest@gmail.com>
2025-11-15 20:07:21 -0500

    updated router.js

--- server/router.js
+++ server/router.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const controllers = require('./controllers');
 const mid = require('./middleware');
 
@@ -5,7 +6,6 @@ const router = (app) => {
     app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
     app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
-    app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
     app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 
     app.get('/logout', mid.requiresLogin, controllers.Account.logout);

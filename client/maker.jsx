35a160c60a289c357c80f5b131527d2e7973e466
mksigrest <mksigrest@gmail.com>
2025-11-15 21:47:22 -0500

    Added DomoForm #36

--- client/maker.jsx
+++ client/maker.jsx
@@ -18,3 +18,21 @@ const handleDomo = (e, onDomoAdded) => {
     helper.sendPost(e.target.action, { name, age }, onDomoAdded);
     return false;
 }
+
+const DomoForm = (props) => {
+    return (
+        <form id="domoForm"
+            onSubmit={(e) => handleDomo(e, props.triggerReload)}
+            name="domoForm"
+            action="/maker"
+            method="POST"
+            className="domoForm"
+        >
+            <label htmlFor="name">Name: </label>
+            <input id="domoName" type="text" name="name" placeholder="Domo Name" />
+            <label htmlFor="age">Age: </label>
+            <input id="domoAge" type="number" min="0" name="age" />
+            <input className="makeDomoSubmit" type="submit" value="Make Domo" />
+        </form>
+    );
+};

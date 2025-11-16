3740fdba783ba8c02c1a88a8339f8e918a2a93cf
mksigrest <mksigrest@gmail.com>
2025-11-15 21:42:26 -0500

    added base maker.jsx

---
+++ client/maker.jsx
@@ -0,0 +1,20 @@
+const helper = require('./helper.js');
+const React = require('react');
+const { useState, useEffect } = React;
+const { createRoot } = require('react-dom/client');
+
+const handleDomo = (e, onDomoAdded) => {
+    e.preventDefault();
+    helper.hideError();
+
+    const name = e.target.querySelector('#domoName').value;
+    const age = e.target.querySelector('#domoAge').value;
+
+    if (!name || !age) {
+        helper.handleError('All fields are required');
+        return false;
+    }
+
+    helper.sendPost(e.target.action, { name, age }, onDomoAdded);
+    return false;
+}

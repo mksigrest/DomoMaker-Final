7656b9f3fc2d44319da8c292303b1a02e8c6c888
mksigrest <mksigrest@gmail.com>
2025-11-15 21:55:51 -0500

    added domoList function

--- client/maker.jsx
+++ client/maker.jsx
@@ -36,3 +36,40 @@ const DomoForm = (props) => {
         </form>
     );
 };
+
+const DomoList = (props) => {
+    const [domos, setDomos] = useState(props.domos);
+
+    useEffect(() => {
+        const loadDomosFromServer = async () => {
+            const response = await fetch('/getDomos');
+            const data = await response.json();
+            setDomos(data.domos);
+        };
+        loadDomosFromServer();
+    }, [props.reloadDomos]);
+
+    if (domos.length === 0) {
+        return (
+            <div className="domoList">
+                <h3 className="emptyDomo">No Domos Yet!</h3>
+            </div>
+        );
+    }
+
+    const domoNodes = domos.map(domo => {
+        return (
+            <div key={domo.id} className="domo">
+                <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
+                <h3 className="domoName">Name: {domo.name}</h3>
+                <h3 className="domoAge">Age: {domo.age}</h3>
+            </div>
+        );
+    });
+
+    return (
+        <div className="domoList">
+            {domoNodes}
+        </div>
+    );
+};

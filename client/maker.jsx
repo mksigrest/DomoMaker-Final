905d78a7ea7e29a46a271495c0bec812c4e91bb2
mksigrest <mksigrest@gmail.com>
2025-11-16 15:51:11 -0500

    updated domoNodes

--- client/maker.jsx
+++ client/maker.jsx
@@ -66,6 +66,7 @@ const DomoList = (props) => {
                 <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
                 <h3 className="domoName">Name: {domo.name}</h3>
                 <h3 className="domoAge">Age: {domo.age}</h3>
+                <h3 className="domoLevel">Level: {domo.level}</h3>
             </div>
         );
     });

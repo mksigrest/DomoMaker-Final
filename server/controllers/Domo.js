e99c40e445b9277bd0c8a14acbf693d8468a7713
mksigrest <mksigrest@gmail.com>
2025-11-15 21:28:51 -0500

    updated domo.js #32

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -25,16 +25,8 @@ const makeDomo = async (req, res) => {
     }
 }
 
-const makerPage = async (req, res) => {
-    try {
-        const query = { owner: req.session.account._id };
-        const docs = await Domo.find(query).select('name age').lean().exec();
-
-        return res.render('app', { domos: docs });
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ error: 'Error retrieving domos!' });
-    }
+const makerPage = (req, res) => {
+    return res.render('app');
 };
 
 module.exports = {

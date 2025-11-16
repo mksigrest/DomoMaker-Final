cae510d4260f59f1b3f9dc6365a7c82c1c9b5b2d
mksigrest <mksigrest@gmail.com>
2025-11-15 20:38:16 -0500

    added signupWindow

--- client/login.jsx
+++ client/login.jsx
@@ -57,3 +57,23 @@ const LoginWindow = (props) => {
         </form>
     );
 };
+
+const signupWindow = (props) => {
+    return (
+        <form id="signupForm"
+            name="signupForm"
+            onSubmit={handleSignup}
+            action="/signup"
+            method="POST"
+            className="mainForm"
+        >
+            <label htmlFor="username">Username: </label>
+            <input id="user" type="text" name="username" placeholder="username" />
+            <label htmlFor="pass">Password: </label>
+            <input id="pass" type="password" name="pass" placeholder="password" />
+            <label htmlFor="pass">Password: </label>
+            <input id="pass2" type="password" name="pass2" placeholder="retype password" />
+            <input className="formSubmit" type="submit" value="Sign up" />
+        </form>
+    );
+};

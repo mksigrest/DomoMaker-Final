0f4561092f0f5997b3bd84bf7956b21b52f0b6e7
mksigrest <mksigrest@gmail.com>
2025-11-16 15:44:36 -0500

    started controllers level implimentation

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -2,20 +2,21 @@ const models = require('../models');
 const Domo = models.Domo;
 
 const makeDomo = async (req, res) => {
-    if (!req.body.name || !req.body.age) {
-        return res.status(400).json({ error: 'Both name and age are required!' });
+    if (!req.body.name || !req.body.age || !req.body.level) {
+        return res.status(400).json({ error: 'Name, age, and level are all required!' });
     }
 
     const domoData = {
         name: req.body.name,
         age: req.body.age,
+        level: req.body.level,
         owner: req.session.account._id,
     };
 
     try {
         const newDomo = new Domo(domoData);
         await newDomo.save();
-        return res.status(201).json({ name: newDomo.name, age: newDomo.age });
+        return res.status(201).json({ name: newDomo.name, age: newDomo.age, level: newDomo.level });
     } catch (err) {
         console.log(err);
         if (err.code === 11000) {
